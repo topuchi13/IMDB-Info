1d54349a4fa2fedd02e92e658adb427d5106791f
topuchi13 <nicolas1213@gmail.com>
2021-10-21 00:05:47 +0400

    Small UI adjustments

---
+++ IMDB Info/Presenter/DetailsView/DetailsView.swift
@@ -0,0 +1,33 @@
+//
+//  DetailsView.swift
+//  DetailsView
+//
+//  Created by Nika Topuria on 20.10.21.
+//
+
+import UIKit
+import Kingfisher
+
+class DetailsView: UIViewController {
+    let imagePrefix = "https://image.tmdb.org/t/p/w500/"
+    @IBOutlet var mainImage: UIImageView!
+    @IBOutlet var movieTitle: UILabel!
+    @IBOutlet var movieScore: UILabel!
+    @IBOutlet var movieDescription: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func makeView(_ movie: Movie) {
+        mainImage.kf.setImage(with: URL(string: "\(imagePrefix)\(movie.largeImage)"))
+        movieTitle.text = movie.movieTitle
+        movieScore.text = "IMDB Score: \(String(movie.score))"
+        movieDescription.text = "Description: \(movie.overview)"
+    }
+
+    
+
+}
